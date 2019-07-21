a7dd93a16cfebd8357c537a80a619b0391b50687
Rumeth <Rumeth@users.noreply.github.com>
2019-07-21 20:34:26 +0530

    Sanitizer

---
+++ src/app/sanitizer.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Pipe({
+  name: 'sanitize'
+})
+
+export class SanitizerPipe implements PipeTransform {
+  constructor(private domSanitizer: DomSanitizer) { }
+
+  transform(html) {
+    return this.domSanitizer.bypassSecurityTrustUrl(html);
+  }
+}
