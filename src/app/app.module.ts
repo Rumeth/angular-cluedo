8d6c2d273cbaa480cdd000b274d74f959458a7d6
Rumeth <Rumeth@gmail.com>
2019-07-22 10:15:11 +0530

    Dependency Module Added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
-import { SanitizerPipe } from './sanitizer';
+import { SanitizerPipe } from '../pipe/sanitizer';
 
-@NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent, SanitizerPipe ],
-  bootstrap:    [ AppComponent ]
-})
-export class AppModule { }
+@NgModule ( {
+                imports      : [
+                    BrowserModule ,
+                    HttpClientModule
+                ] ,
+                declarations : [
+                    AppComponent ,
+                    SanitizerPipe
+                ] ,
+                bootstrap    : [
+                    AppComponent
+                ]
+            } )
+
+export class AppModule
+{
+}
