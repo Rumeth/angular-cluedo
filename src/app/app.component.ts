a900f88ed7665995aa78379bd22d0aa68c48f0f2
Rumeth <rumeth@gmail.com>
2019-07-22 13:29:38 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,213 +1,189 @@
 import { Component } from '@angular/core';
 import { Status } from '../constants/status';
 import { AppService } from './app.service';
-import { Card , CardStatus } from '../model/card.interface';
+import { Card, CardStatus } from '../model/card.interface';
 import { Player } from '../model/player.interface';
 import { Session } from '../model/session.interface';
 import { Types } from '../model/types.interface';
 
-@Component ( {
-                 selector    : 'my-app' ,
-                 templateUrl : './app.component.html' ,
-                 styleUrls   : [ './app.component.css' ]
-             } )
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
 
-export class AppComponent
-{
-    session : Session;
+export class AppComponent {
+  session: Session;
 
-    player : Player;
+  player: Player;
 
-    types : Types[];
+  types: Types[];
 
-    Status = Status;
+  Status = Status;
 
-    constructor ( private appService : AppService )
-    {
-    }
+  constructor(private appService: AppService) {
+  }
 
-    ngOnInit () : void
-    {
-        this.appService.getSession ()
-            .subscribe ( ( session : Session ) =>
-                         {
-                             this.session = session;
+  ngOnInit(): void {
+    this.getSession();
+  }
 
-                             this.session.players = this.appService.shuffle ( this.session.players );
-                         } );
+  getSession(): void {
+    this.appService.getSession()
+      .subscribe((session: Session) => {
+        this.session = session;
 
-        this.appService.getPlayer ()
-            .subscribe ( ( player : Player ) =>
-                         {
-                             this.player = player;
-                         } );
+        this.session.players = this.appService.shuffle(this.session.players);
 
-        this.getPieces ();
-    }
+        this.getPlayer();
+      });
+  }
 
-    getPieces () : void
-    {
-        this.appService.getPieces ()
-            .subscribe ( ( types : Types[] ) =>
-                         {
-                             const shuffledTypes : Types[] = this.appService.shuffle ( types );
+  getPlayer(): void {
+    this.appService.getPlayer()
+      .subscribe((player: Player) => {
+        this.player = player;
 
-                             const playerHand : number[] = [];
+        this.getPieces();
+      });
+  }
 
-                             for ( const hand of this.player.hand )
-                             {
-                                 playerHand.push ( hand.id );
-                             }
+  getPieces(): void {
+    this.appService.getPieces()
+      .subscribe((types: Types[]) => {
+        const shuffledTypes: Types[] = this.appService.shuffle(types);
 
-                             for ( const type of shuffledTypes )
-                             {
-                                 type.pieces = this.appService.shuffle ( type.pieces );
+        const playerHand: number[] = [];
 
-                                 type.pieces.map ( ( piece : Card ) =>
-                                                   {
-                                                       piece.status = [];
-
-                                                       piece.frozen = false;
-
-                                                       for ( const player of this.session.players )
-                                                       {
-                                                           const cardStatus : CardStatus = {
-                                                               player : player.hash ,
-                                                               status : Status.EMPTY ,
-                                                               frozen : false
-                                                           };
-
-                                                           if ( playerHand.indexOf ( piece.id ) > -1 )
-                                                           {
-                                                               cardStatus.status = Status.NO;
-                                                               cardStatus.frozen = true;
-
-                                                               piece.frozen = true;
-
-                                                               if ( player.hash === this.player.hash )
-                                                               {
-                                                                   cardStatus.status = Status.YES;
-                                                               }
-                                                           }
-
-                                                           piece.status.push ( cardStatus );
-                                                       }
-                                                   } );
-                             }
-
-                             this.types = shuffledTypes;
-                         } );
-    }
-
-    toggleStatus ( cardStatus : CardStatus , piece : Card ) : void
-    {
-        if ( !cardStatus.frozen )
-        {
-            const status : number[] = Object.values ( Status )
-                                            .filter ( ( value : number ) => !isNaN ( value ) );
-
-            let index : number = status.indexOf ( cardStatus.status );
-
-            if ( index > -1 )
-            {
-                index++;
-
-                if ( index >= status.length )
-                {
-                    index = 0;
-                }
-
-                cardStatus.status = status[ index ];
-            }
-
-            if ( cardStatus.status === Status.YES )
-            {
-                this.freezePiece ( piece );
-            }
-            else
-            {
-                this.unfreezePiece ( piece );
-            }
+        for (const hand of this.player.hand) {
+          playerHand.push(hand.id);
         }
-    }
 
-    freezePiece ( piece : Card ) : void
-    {
-        for ( const cardStatus of piece.status )
-        {
-            if ( cardStatus.status !== Status.YES )
-            {
+        for (const type of shuffledTypes) {
+          type.pieces = this.appService.shuffle(type.pieces);
+
+          type.pieces.map((piece: Card) => {
+            piece.status = [];
+
+            piece.frozen = false;
+
+            for (const player of this.session.players) {
+              const cardStatus: CardStatus = {
+                player: player.hash,
+                status: Status.EMPTY,
+                frozen: false
+              };
+
+              if (playerHand.indexOf(piece.id) > -1) {
+                cardStatus.status = Status.NO;
                 cardStatus.frozen = true;
+
+                piece.frozen = true;
+
+                if (player.hash === this.player.hash) {
+                  cardStatus.status = Status.YES;
+                }
+              }
+
+              piece.status.push(cardStatus);
             }
+          });
         }
 
-        piece.frozen = true;
+        this.types = shuffledTypes;
+      });
+  }
+
+  toggleStatus(cardStatus: CardStatus, piece: Card): void {
+    if (!cardStatus.frozen) {
+      const status: number[] = Object.values(Status)
+        .filter((value: number) => !isNaN(value));
+
+      let index: number = status.indexOf(cardStatus.status);
+
+      if (index > -1) {
+        index++;
+
+        if (index >= status.length) {
+          index = 0;
+        }
+
+        cardStatus.status = status[index];
+      }
+
+      if (cardStatus.status === Status.YES) {
+        this.freezePiece(piece);
+      }
+      else {
+        this.unfreezePiece(piece);
+      }
+    }
+  }
+
+  freezePiece(piece: Card): void {
+    for (const cardStatus of piece.status) {
+      if (cardStatus.status !== Status.YES) {
+        cardStatus.frozen = true;
+      }
     }
 
-    unfreezePiece ( piece : Card ) : void
-    {
-        for ( const cardStatus of piece.status )
-        {
-            cardStatus.frozen = false;
-        }
+    piece.frozen = true;
+  }
 
-        piece.frozen = false;
+  unfreezePiece(piece: Card): void {
+    for (const cardStatus of piece.status) {
+      cardStatus.frozen = false;
     }
 
-    getStatusIcon ( cardStatus : CardStatus ) : string
-    {
-        const styles : string[] = [];
+    piece.frozen = false;
+  }
 
-        switch ( cardStatus.status )
-        {
-            case Status.YES:
-                styles.push ( 'fa-check' );
-                styles.push ( 'text-success' );
-                break;
-            case Status.NO:
-                styles.push ( 'fa-times' );
-                styles.push ( 'text-danger' );
-                break;
-            case Status.DOUBT:
-                styles.push ( 'fa-question' );
-                styles.push ( 'text-warning' );
-                break;
-            default:
-        }
+  getStatusIcon(cardStatus: CardStatus): string {
+    const styles: string[] = [];
 
-        return styles.join ( ' ' );
+    switch (cardStatus.status) {
+      case Status.YES:
+        styles.push('fa-check');
+        styles.push('text-success');
+        break;
+      case Status.NO:
+        styles.push('fa-times');
+        styles.push('text-danger');
+        break;
+      case Status.DOUBT:
+        styles.push('fa-question');
+        styles.push('text-warning');
+        break;
+      default:
     }
 
-    getPieceStatus ( piece : Card ) : string
-    {
-        const styles : string[] = [];
+    return styles.join(' ');
+  }
 
-        const existingIndex : number = piece.status.findIndex ( ( cardStatus : CardStatus ) =>
-                                                                {
-                                                                    return cardStatus.status === Status.YES;
-                                                                } );
+  getPieceStatus(piece: Card): string {
+    const styles: string[] = [];
 
-        if ( existingIndex > -1 )
-        {
-            styles.push ( 'text-success' );
-            styles.push ( 'line-through' );
-        }
+    const existingIndex: number = piece.status.findIndex((cardStatus: CardStatus) => {
+      return cardStatus.status === Status.YES;
+    });
 
-        const shortlisted : number = piece.status.reduce ( ( total : number , cardStatus : CardStatus ) =>
-                                                           {
-                                                               if ( cardStatus.status === Status.NO )
-                                                               {
-                                                                   return total + 1;
-                                                               }
-
-                                                               return total;
-                                                           } , 0 );
-
-        if ( shortlisted === piece.status.length )
-        {
-            styles.push ( 'text-danger' );
-        }
-
-        return styles.join ( ' ' );
+    if (existingIndex > -1) {
+      styles.push('text-success');
+      styles.push('line-through');
     }
+
+    const shortlisted: number = piece.status.reduce((total: number, cardStatus: CardStatus) => {
+      if (cardStatus.status === Status.NO) {
+        return total + 1;
+      }
+
+      return total;
+    }, 0);
+
+    if (shortlisted === piece.status.length) {
+      styles.push('text-danger');
+    }
+
+    return styles.join(' ');
+  }
 }
